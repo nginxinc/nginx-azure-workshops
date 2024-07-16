2f50b29b65546e691443f115b3edb78b862ea12e
Shouvik <shouvik.dutta96@gmail.com>
2024-07-16 15:29:12 -0500

    updated init.sh script

--- labs/lab2/init.sh
+++ labs/lab2/init.sh
@@ -11,3 +11,14 @@ sudo apt-get install -y docker.io
 # Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# Create a new directory
+cd $HOME
+mkdir cafe
+
+# Download docker compose file
+cd cafe
+wget https://raw.githubusercontent.com/nginxinc/nginx-azure-workshops/main/labs/lab2/docker-compose.yml
+
+# Run docker compose to create the containers
+sudo docker-compose up -d
